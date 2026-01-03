e653030f2a94de89c65fb9f464ad4a326bd7dcb7
PlasmaVolt <61515425+PlasmaVolt@users.noreply.github.com>
2026-01-02 21:41:05 -0800

    feat: spider closes upon finding desired link

--- wiki_racer/pipelines.py
+++ wiki_racer/pipelines.py
@@ -3,11 +3,28 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem, CloseSpider
+import json
 
 
 class WikiRacerPipeline:
+    
+    itemlist = []
+    
+    def open_spider(self, spider):
+        self.file = open("items.jsonl", "w")
+        
+    def close_spider(self, spider):
+        self.file.close()
+    
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item).asdict()
+        if item in self.itemlist:
+            raise DropItem
+        self.itemlist.append(item)
+        
+        line = json.dumps(adapter) + "\n"
+        self.file.write(line)
         return item
--- wiki_racer/spiders/wikipedia.py
+++ wiki_racer/spiders/wikipedia.py
@@ -1,13 +1,28 @@
 import scrapy
+from collections import deque
 
+from scrapy.exceptions import DropItem, CloseSpider
 
 class WikipediaSpider(scrapy.Spider):
+    custom_settings = {
+        'CLOSESPIDER_PAGECOUNT': 10,
+        'DEPTH_PRIORITY': 1,
+        'SCHEDULER_DISK_QUEUE': 'scrapy.squeues.PickleFifoDiskQueue',
+        'SCHEDULER_MEMORY_QUEUE': 'scrapy.squeues.FifoMemoryQueue',
+    }
     name = "wikipedia"
     allowed_domains = ["wikipedia.org"]
     start_urls = ["https://en.wikipedia.org/wiki/Measure_(mathematics)"]
 
     def parse(self, response):
-        for link in response.xpath('//p/a/@href').getall():
+        links = response.xpath('//p/a/@href').getall()
+        for link in links:
             yield {
                 'link': link
             }
+            if link == "/wiki/Mathematics":
+                raise CloseSpider('found!')
+        
+        
+        next_page = response.urljoin(links[0])
+        yield scrapy.Request(next_page, callback=self.parse)
