0c5b6aa7301f9351799c9bf133dca1dd24500c3f
PlasmaVolt <61515425+PlasmaVolt@users.noreply.github.com>
2026-01-06 15:23:25 -0800

    feat: use sentence transformers to rank possible next links on semantic similarity

---
+++ src/wiki_racer/__init__.py
@@ -0,0 +1,10 @@
+import nltk
+import os
+
+# Configure nltk data path
+NLTK_DATA_PATH = './.venv/share/nltk_data/'
+nltk.data.path.insert(0, NLTK_DATA_PATH)
+
+# Download if not already present
+if not os.path.exists(os.path.join(NLTK_DATA_PATH, 'tokenizers/punkt_tab')):
+    nltk.download('punkt_tab', download_dir=NLTK_DATA_PATH)
